6815e5bb1e06d8e7a89a227c91e102c70303fd30
marismcg <mcguinness.m@northeastern.edu>
2022-12-02 12:40:57 -0500

    add connection string

--- app.js
+++ app.js
@@ -1,5 +1,4 @@
 import mongoose from "mongoose";
-mongoose.connect('mongodb://localhost:27017/tuiter');
 import express from 'express';
 import cors from 'cors';
 import HelloController
@@ -8,6 +7,11 @@ import UserController
     from "./controllers/users/users-controller.js";
 import TuitsController
     from "./controllers/tuits/tuits-controller.js";
+process.env.DB_CONNECTION_STRING = "mongodb+srv://marismcg:<password>@cluster0.uw437mi.mongodb.net/?retryWrites=true&w=majority";
+const CONNECTION_STRING = process.env.DB_CONNECTION_STRING
+    || 'mongodb://localhost:27017/tuiter';
+mongoose.connect(CONNECTION_STRING);
+mongoose.connect('mongodb://localhost:27017/tuiter');
 const app = express()
 app.use(cors());
 app.use(express.json());
